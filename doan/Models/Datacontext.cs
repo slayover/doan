a01075e3f87d092e1cd7cb341d016f189d41b9aa
ADMIN <ADMIN@DESKTOP-FEMLRGT>
2023-12-13 15:24:52 +0700

    Add project files.

---
+++ doan/Models/Datacontext.cs
@@ -0,0 +1,15 @@
+﻿using doan.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace doan.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
